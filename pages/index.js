59fce83dcdbd0bd5bc24714d8d2566f11bc68557
Nick Millet <NMillet@daytonfreight.com>
2021-03-22 11:39:53 -0400

    Added a description. Also,  styled products

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/Home.module.scss";
 import CartContainer from "../containers/CartContainer";
 import ProductsContainer from "../containers/ProductsContainer";
-import { Button, Typography } from "antd";
+import { Typography } from "antd";
 import { initializeStore } from "../store";
 import { receiveProducts } from "../actions";
 import { getAll } from "../services/products";
@@ -11,12 +11,17 @@ const { Title } = Typography;
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ alignItems: "center" }}>
       <Title className={styles.title}>Welcome to Macy's Bakery</Title>
       <div className={styles.description}>
-        <a href="https://ide.geeksforgeeks.org/">
-          <Button>About Us</Button>
-        </a>
+        <p>
+          100% of profits go to the Lily Orphan Care Centers (LOCC). The LOCC
+          are a part of the Chinese Children Adoption International organization
+          (CCAI). The Lily Orphan Care centers strive to improve care and living
+          conditions in Chinese orphanages. This charity is import to me because
+          the CCAI is the agency that I was adopted from. With your help, I hope
+          to make a difference in these orphans' lives.
+        </p>
       </div>
       <div>
         <ProductsContainer />
